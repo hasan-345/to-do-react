f4c4733be1aafaa5a89df871d4169ffd13ec072b
Ahtisham-ul-hassan <164784447+hasan-345@users.noreply.github.com>
2024-04-13 23:40:28 +0500

    Update Todocontext.js

--- realtodo/src/context/Todocontext.js
+++ realtodo/src/context/Todocontext.js
@@ -1,14 +1,14 @@
 import { createContext,useContext } from "react";
 
 export const TodoContext = createContext({
-    todos:[
+    todos:[ // this is object in which we store all info about todo items
         {
             id: 1,
             todo: "Hello every body",
             complete: false
         }
-    ],
-    addTodo: (todo)=>{},
+    ], //this is context api in which we set all values of to do list
+    addTodo: (todo)=>{}, // it is addtodo function which is used to add todo items where parameters todo means value of input
     updateTodo: (id,todo)=>{},
     toggleComplete : (id)=>{},
     deleteTodo: (id)=>{}
